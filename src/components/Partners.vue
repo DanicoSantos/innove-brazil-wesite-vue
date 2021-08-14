e6cccb06f2c33ba3e3259912fcb08ccf6e09f93e
Daniel Santos <dados.dan.santos@gmail.com>
2021-08-14 13:54:48 -0300

    feat: add base structure to partners section

---
+++ src/components/Partners.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="partners">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-3">
+          <figure class="image">
+            <img class="is-rounded" src="https://via.placeholder.com/150" />
+          </figure>
+        </div>
+
+        <div class="column is-3">
+          <figure class="image">
+            <img class="is-rounded" src="https://via.placeholder.com/150" />
+          </figure>
+        </div>
+
+        <div class="column is-3">
+          <figure class="image">
+            <img class="is-rounded" src="https://via.placeholder.com/150" />
+          </figure>
+        </div>
+
+        <div class="column is-3">
+          <figure class="image">
+            <img class="is-rounded" src="https://via.placeholder.com/150" />
+          </figure>
+        </div>
+      </div>
+
+      <div class="colums is-centered">
+        <div class="column">
+          <router-link to="/parceiros" class="button is-info mt-6"
+            >Parceiros</router-link
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.partners {
+  margin: 15rem auto;
+}
+</style>
+
+<script>
+export default {
+  name: "Partners",
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="home">
     <Carousel />
+    <Partners />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Carousel from "@/components/Carousel";
+import Partners from "@/components/Partners";
 
 export default {
   name: "Home",
-  components: { Carousel },
+  components: {
+    Carousel,
+    Partners,
+  },
 };
 </script>
