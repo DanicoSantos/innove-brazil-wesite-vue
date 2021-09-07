6c29d62eaf8a1c7036dbfed442d82d4b9698641c
Daniel Santos <dados.dan.santos@gmail.com>
2021-09-07 16:24:05 -0300

    feat: add images to public folder

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="carousel">
     <section class="hero carousel__hero is-fullheight">
-      <img
-        :src="currentImage"
-        style="height: 100%"
-        alt="Some image"
-      />
+      <img :src="currentImage" style="height: 100%" alt="Some image" />
       <div class="actions">
         <span @click="prevImage" class="prev">
           <i class="fas fa-chevron-left"></i>
@@ -78,10 +74,42 @@
   height: 15px;
   width: 15px;
   margin: 0 2px;
-  background-color: #bbb;
+  background-color: rgba($color: #000, $alpha: 0.4);
   border-radius: 50%;
   display: inline-block;
   transition: background-color 0.6s ease;
+}
+
+.active .dot, .dot:hover {
+  background-color: #fff;
+}
+
+.dots {
+  position: absolute;
+  right: 5rem;
+  top: 50%;
+  padding: 3rem;
+  background: rgba($color: #fff, $alpha: 0.20);
+  border-radius: 0.5rem;
+  z-index: 10001;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4}
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4}
+  to {opacity: 1}
 }
 </style>
 
@@ -91,8 +119,49 @@ export default {
   data() {
     return {
       heroSize: "",
-      items: [],
+      images: [
+        {
+          id: 1,
+          src: "/img/banner-home.jpg",
+        },
+        {
+          id: 2,
+          src: "/img/banner-about.jpg",
+        },
+      ],
+      activeImage: 0,
     };
+  },
+  computed: {
+    // currentImage gets called whenever activeImage changes
+    // and is the reason why we don't have to worry about the
+    // big image getting updated
+    currentImage() {
+      return this.images[this.activeImage].src;
+    },
+  },
+  methods: {
+    // Go forward on the images array
+    // or go at the first image if you can't go forward :/
+    nextImage() {
+      var active = this.activeImage + 1;
+      if (active >= this.images.length) {
+        active = 0;
+      }
+      this.activateImage(active);
+    },
+    // Go backwards on the images array
+    // or go at the last image
+    prevImage() {
+      var active = this.activeImage - 1;
+      if (active < 0) {
+        active = this.images.length - 1;
+      }
+      this.activateImage(active);
+    },
+    activateImage(imageIndex) {
+      this.activeImage = imageIndex;
+    },
   },
 };
 </script>
